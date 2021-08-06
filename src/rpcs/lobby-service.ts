992b61a0929ce2985d6eb915253a4abe3dd7df3c
OlegGedzjuns <oleggedzjuns@gmail.com>
2021-08-06 11:50:35 +0300

    Updated error handling, updated match settings schema

--- src/rpcs/lobby-service.ts
+++ src/rpcs/lobby-service.ts
@@ -7,13 +7,13 @@ function rpcLobbyInvite(context: nkruntime.Context, logger: nkruntime.Logger, nk
     }
 
     if (!payloadObject.matchId) {
-        return new NakamaError(ERROR_TYPES.WRONG_PARAMS, 'No matchId provided').toString();
+        throw new NakamaError(ERROR_TYPES.WRONG_PARAMS, 'No matchId provided').toString();
     }
 
     const users: nkruntime.User[] = nk.usersGetUsername([payloadObject.username]);
 
     if (!users[0]) {
-        return new NakamaError(ERROR_TYPES.WRONG_PARAMS, 'User not found').toString();
+        throw new NakamaError(ERROR_TYPES.WRONG_PARAMS, 'User not found').toString();
     }
 
     nk.notificationSend(users[0].userId, 'You are invited to lobby', { matchId: payloadObject.matchId }, NOTIFICATION_TYPES.LOBBY_INVITE, context.userId, true);
--- src/rpcs/match-service.ts
+++ src/rpcs/match-service.ts
@@ -1,5 +1,5 @@
 function rpcCreateMatch(context: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payload: string): string {
     const payloadObject = JSON.parse(payload);
-    const matchId = nk.matchCreate(payloadObject.matchType, payloadObject.settings);
+    const matchId = nk.matchCreate(payloadObject.type, payloadObject.settings);
     return JSON.stringify({ matchId });
 }
